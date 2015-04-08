c04a56dc24ff19c9d64d81f1c850717b5c6fb58d
Noah Sluss <slussnoah@gmail.com>
2015-04-08 13:47:36 -0700

    complete walkthrough of section 3 todo: complete exercises

--- parser/simpleParser.hs
+++ parser/simpleParser.hs
@@ -1,15 +1,55 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
+
+data LispVal = Atom String
+  | List [LispVal] -- proper list
+  | DottedList [LispVal] LispVal -- improper list
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+  <|> parseString
+  <|> parseNumber
 
 main :: IO ()
 main = do 
   args <- getArgs
   putStrLn (readExpr (args !! 0))
+
+-- >> if the actions don't return a value
+-- >>= if you immediately pass the return to the next action
+-- do -notation otherwise
