699f123920ff0a7c7f6fa619988f286e8a76ea8e
Noah Sluss <slussnoah@gmail.com>
2015-07-12 17:04:43 -0700

    Exercises are in seperate commits. New ones overwrite the old ones in the future I'll change this to include all exercises up to that point in each commit (where reasonable).

--- ch01/Main.hs
+++ ch01/Main.hs
@@ -2,12 +2,12 @@ module Main where
 import System.Environment
 
 
---Change the program so it performs a simple arithmetic operation on the two 
---arguments and prints out the result. You can use read to convert a string to a 
---number, and show to convert a number back into a string.
---Play around with different operations.
+--getLine is an IO action that reads a line from the console and returns it as
+--a string. Change the program so it prompts for a name, reads the name, and then
+--  prints that instead of the command line value
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn (show . foldr (+) 0 $ map read args)
+  putStrLn "What's your name?"
+  name <- getLine
+  putStrLn ("Hi, " ++ name ++ "!")
